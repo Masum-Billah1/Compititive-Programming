08b8ea9aa378918e4cc245c70793878b3f2e840f
Masum-Billah1 <53857306+Masum-Billah1@users.noreply.github.com>
2022-04-05 06:30:23 +0600

    depth first search
    reference : 
    1.https://www.youtube.com/watch?v=CVe4NypqwSE
    2.https://www.youtube.com/watch?v=-1HUUMnCRjs

---
+++ dfs.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+int mat[100][100],color[100];
+int node,edge,i,j;
+
+void dfs(int x){
+	color[x]=1;
+	for(i=0;i<node;i++){
+		if(mat[x][i]==1 && color[i]==0){
+			dfs(i);
+		}
+	}
+	color[x]=2;
+}
+int main(){
+	int x,y,sum=0;
+	cin >> node;
+	cin >> edge;
+	for(i=0;i<edge;i++){
+		cout << "\nedge from u -> v : ";
+		cin >> x;
+		cin >> y;
+		mat[x][y]=1;
+		mat[y][x]=1;
+	}
+	for(i=0;i<node;i++)
+		color[i]=0;
+	for(j=0;j<node;j++){
+		if(color[i]==0)
+			dfs(j);
+	}
+	
+	for(i=0;i<node;i++){
+		if(color[i]!=0)
+			sum++;
+	}
+	if(sum == node)
+		cout <<"All the nodes are visited"<<endl;
+	else
+		cout <<"All the nodes are not visited"<<endl;
+}
